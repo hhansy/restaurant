a243f53d995d75549ab922ae056cc7708447fba4
hhansy <maksik30082008@gmail.com>
2024-06-30 13:03:17 +0300

    юю

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 let card_list = document.querySelector(".my-card")
 
+async function getData() {
+    const response = await fetch("https://api.zerosheets.com/v1/qbj", {
+        method: "GET",
+        headers: {
+            Authorization: "Bearer TEDEptOEXKS00SwqqD0CPC9kdX1MTQoC"
+        }
+    });
+    const data = await response.json();
+
+    // will return an array of objects with the _lineNumber
+    return data;
+}
 
 async function getProducts() {
     // Виконуємо запит до файлу "store_db.json" та очікуємо на відповідь
